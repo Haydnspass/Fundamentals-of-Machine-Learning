68598a988dbc231357500a13fb504303713c37a7
Lucas-Rapahel Müller <gitdev@LRM.photo>
2017-11-28 16:59:34 +0100

    Relocate

--- sheet04/test_generative_models.py
+++ sheet04/test_generative_models.py
@@ -2,9 +2,9 @@ import h5py
 from generative_models import GenerativeBayes
 
 def test_data_availability():
-        f = h5py.File("data/digits.h5")
+        f = h5py.File("sheet04/data/digits.h5")
         images = f["images"].value
         labels = f["labels"].value
         f.close()
 
-        assert images.shape[1:] == (9,9) && images.shape[0] == labels.shape[0]
+        assert images.shape[1:] == (9,9) and images.shape[0] == labels.shape[0]
